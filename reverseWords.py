addd5af2d4e12beab59811266048e922df31348a
mgud1989 <m.gud1989@gmail.com>
2020-03-15 11:24:16 -0300

    refactoring

--- reverseWords.py
+++ reverseWords.py
@@ -1,15 +1,11 @@
 def reverse_words(text):
 
     words = text.split()
-    
-
-    #letter.append(text[])
 
     return words
 
 def recorre_letras(words):
     
-<<<<<<< HEAD
     letters = []
     for w in words:
         letters.append("letter")
@@ -18,13 +14,13 @@ def recorre_letras(words):
 
 def reverse (palabra):
 
-    p_reverse = []
+    #p_reverse = []
     s_reverse = ""
     i=(len(palabra) -1)
 
     while i > -1:
 
-        p_reverse.append(palabra[i])
+        #p_reverse.append(palabra[i])
         s_reverse += palabra[i]
         i-=1
 
@@ -33,17 +29,3 @@ def reverse (palabra):
 print (list(recorre_letras(reverse_words("test test test"))))
 
 print (reverse("probando"))
-=======
-    new_words = []
-    
-    for w in words:
-        for l in w:
-            yield new_words.append("letter")
-    
-        
-
-    return list(new_words)
-
-
-print (list(recorre_letras(reverse_words("test test test"))))
->>>>>>> 92a891955f5107eedaa3d0e27f47dce167f00864
