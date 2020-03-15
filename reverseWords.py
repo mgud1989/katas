0521d6e621efd52de46e1f0f84be3caade4f1dfc
mgud1989 <m.gud1989@gmail.com>
2020-03-15 11:11:03 -0300

    add reverse method to master

--- reverseWords.py
+++ reverseWords.py
@@ -9,6 +9,31 @@ def reverse_words(text):
 
 def recorre_letras(words):
     
+<<<<<<< HEAD
+    letters = []
+    for w in words:
+        letters.append("letter")
+
+    return list(letters)
+
+def reverse (palabra):
+
+    p_reverse = []
+    s_reverse = ""
+    i=(len(palabra) -1)
+
+    while i > -1:
+
+        p_reverse.append(palabra[i])
+        s_reverse += palabra[i]
+        i-=1
+
+    return s_reverse
+
+print (list(recorre_letras(reverse_words("test test test"))))
+
+print (reverse("probando"))
+=======
     new_words = []
     
     for w in words:
@@ -21,3 +46,4 @@ def recorre_letras(words):
 
 
 print (list(recorre_letras(reverse_words("test test test"))))
+>>>>>>> 92a891955f5107eedaa3d0e27f47dce167f00864
