230d5721f8d5b90991eee4224d278d69be37bf5d
mgud1989 <m.gud1989@gmail.com>
2020-03-15 10:57:22 -0300

    agregando reverseWords con reverse

---
+++ reverseWords.py
@@ -0,0 +1,23 @@
+def reverse_words(text):
+
+    words = text.split()
+    
+
+    #letter.append(text[])
+
+    return words
+
+def recorre_letras(words):
+    
+    new_words = []
+    
+    for w in words:
+        for l in w:
+            yield new_words.append("letter")
+    
+        
+
+    return list(new_words)
+
+
+print (list(recorre_letras(reverse_words("test test test"))))
